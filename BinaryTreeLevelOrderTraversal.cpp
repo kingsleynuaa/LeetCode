7686d4c8730cfe94fe7b616364f76d36935961c2
Ke Jin <kingsleynuaa@gmail.com>
2016-08-03 21:56:27 +0800

    second

---
+++ BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * 使用队列实现层次遍历
+ * Defition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root == NULL) return result;
+        vector<int> temp;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        TreeNode* tempNode;
+        int flag = 0;
+        int count = 1;
+        while(!q.empty())
+        {
+            temp.clear();
+            flag = 0;
+            for(int i = 0; i < count; i++)  //count表示这一层需要pop几次，就是有几个元素
+            {
+                tempNode = q.front();
+                q.pop();
+                temp.push_back(tempNode->val);
+                cout << tempNode->val << " ";
+            
+                if(tempNode->left != NULL)
+                {
+                    q.push(tempNode->left);
+                    flag++;
+                }
+                
+                if(tempNode->right != NULL)
+                {
+                    q.push(tempNode->right);
+                    flag++;
+                }
+              }
+           
+             count = flag;
+             result.push_back(temp);
+        }
+        
+        return result;
+}
+
+};
